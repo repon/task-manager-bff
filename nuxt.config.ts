dad8976d0ab4a30bb14bb8cc34ecb788a3aaed8b
repon <repon27@yahoo.co.jp>
2025-03-19 18:00:52 +0900

    eslintとprettierの設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
+/* global defineNuxtConfig */
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint']
+  modules: ['@nuxt/eslint'],
 })
